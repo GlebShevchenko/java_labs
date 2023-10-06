8061547c78573c6de02f3e2e38a957bbb5a49933
gleba <gleb.2015.shevchenko@gmail.com>
2023-10-06 16:38:38 +0300

    Некоторые изменения

--- src/MyString.java
+++ src/MyString.java
@@ -15,15 +15,16 @@ public class MyString {
     }
     public
     static void ShowWorkString(){
+        System.out.print("Написать программу, демонстрирующую методы для работы с объектами String\n");
         String defaultStr;
-        MyString[] array_myStr = new MyString[3];
+        MyString[] array_myStr = new MyString[3];//массив строк
         array_myStr[0] = new MyString();
-        array_myStr[1] = new MyString("HelloWorld_2");
+        array_myStr[1] = new MyString("HelloWorld_2");//создание строки из строки
         char[] data = {'a','b','c','d'};
-        array_myStr[2]= new MyString(data);
+        array_myStr[2]= new MyString(data);//создание строки из массива char
         int i;
         for(i = 0; i<3; i++){
-            defaultStr = "myStr" + (i + 1) + ": ";
+            defaultStr = "myStr" + (i + 1) + ": ";//сложение строк
             System.out.print((defaultStr));
             array_myStr[i].Print();
             System.out.print("\n");
